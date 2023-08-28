c14c8f01efd333dfaebf578432173d86a21344ef
webguru0110 <133039683+webguru0110@users.noreply.github.com>
2023-08-28 11:23:30 +0200

    add lagest permutation solution

---
+++ Largest Permutation.java
@@ -0,0 +1,83 @@
+// Java 15
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'largestPermutation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> largestPermutation(int k, List<Integer> arr) {
+        int n = arr.size();
+        Map<Integer, Integer> index = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            index.put(arr.get(i), i);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (k == 0) {
+                break;
+            }
+            
+            if (arr.get(i) == (n - i)) {
+                continue;
+            }
+            
+            int idx = index.get(n - i);
+            index.put(arr.get(i), idx);
+            index.put(n - i, i);
+            
+            Collections.swap(arr, i, idx);
+            k--;
+        }
+        
+        return arr;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.largestPermutation(k, arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
